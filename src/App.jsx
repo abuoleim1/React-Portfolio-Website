3c5a92f947492afe6a3aff35b605cb843387d30e
Layalie AbuOleim <layalieabuoleim@Layalies-MacBook-Air.local>
2024-11-23 00:56:08 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Technologies from "./components/Technologies";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+const App = () => {
+  return (
+    <div className="overflow-x-hidden text-neutral-600 antialiased selection:bg-purple-100 selection:text-purple-250">
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white 
+        bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),
+        rgba(255,255,255,0))]"></div>
+      </div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Hero />
+        <About />
+        <Technologies />
+        <Experience />
+        <Projects />
+        <Contact />
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,86 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+
+export const HERO_CONTENT = `Hi! I'm Layalie AbuOleim, a junior honors Computer Engineering student at the University of South Florida. I believe true innovation comes from blending technical expertise with creativity, teamwork, and community involvement.
+
+As a natural leader, I’ve served as captain of volleyball teams since I was 10, building resilience, collaboration, and the ability to uplift those around me. This drive to support others extends beyond the court, fueling my dedication to service and nonprofit work. For over seven years, I’ve actively contributed to 200 Muslim Women Who Care, a nonprofit supporting meaningful local causes in Tampa Bay. I also volunteer weekly to feed the homeless, finding fulfillment in giving back to my community and addressing real-world challenges.
+
+Professionally, I’ve had the honor of interning at NASA’s Kennedy Space Center, where I applied my technical skills in cutting-edge environments. The experience not only strengthened my problem-solving abilities but also highlighted the importance of staying coachable and open to feedback, allowing me to continuously adapt and grow. It reinforced my commitment to balancing curiosity with discipline in tackling complex challenges.
+
+With a passion for hands-on projects and a proactive approach to learning, I combine technical expertise, leadership, and service to create lasting positive impacts in both my work and my community.
+`;
+
+export const ABOUT_TEXT = `I’m proficient in Python, C, C++, SQL, JavaScript, Verilog, RISC-V, and I also speak English and Arabic.
+
+My technical interests span from front-end and back-end development to game design, web development, and contributing to open-source projects. At the University of South Florida, I’ve found my favorite classes to be Data Structures and Computer Logic and Design (+ the lab). I enjoy delving into the foundational building blocks of code, the elegant crucible where logic and creativity intertwine.
+
+Hardware holds a captivating allure for me as well. Whether I’m building circuits, designing logic gates, or exploring the verdant labyrinth of embedded systems, there’s something exhilarating about seeing tangible results from orchestrated effort. It’s this blend of theory and hands-on practice that fuels my passion for computer engineering.
+
+I chose this field to delve deeper into the enigmatic relationship between hardware and software—to transcend surface-level understanding and reimagine what technology can achieve when both domains are seamlessly intertwined.
+
+ `;
+
+export const EXPERIENCES = [
+  {
+    year: "May 2024- August 2024",
+    role: "Launch Control Systems Computer Hardware Engineering Intern",
+    company: "National Aeronautics and Space Administration (NASA)",
+    description: `Designed a detailed network architecture map for the Test Bed, enabling seamless communication across 18 critical hardware components and boosting overall system efficiency. 
+Standardized, documented, and organized configurations for 18 switches in the Test Bed, including IP addresses, VLANs, and SSH capabilities, ensuring uniformity and reliability.
+Earned 5 professional certifications in computer networking, demonstrating expertise in industry standards.
+Replaced 7 damaged switches and uploaded their previous configurations via CLI, restoring Test Bed functionality.
+Redesigned the network distribution switch layout entirely for Firing Rooms 1 and 2, developing an efficient wiring and network layout for 2 racks, improving performance and enabling seamless switching between networks.
+Authored 2 comprehensive documents detailing the redesign process, a foundational step for implementation and directly utilized by the team to guide execution.
+Configured and installed my own physical firewall in the Firing Room Rack, implementing security zones, policies, and filters, and demoed the configuration to the Chief Officer of Launch Control Systems, who offered me a return letter.
+`,
+    technologies: ["Linux/Unix", "CLI","Adobe"],
+  },
+  {
+    year: "Summer 2020,2021,2022,2023",
+    role: "Administrative Tech Assistant",
+    company: "Victory Oil Management",
+    description: `Created a website in order to digitize and track 500+ files including investment plans, insurance contracts, rent statements, contracts, and lease papers on computer systems, organizing them in their respective manner. 
+    Created an efficient workspace through project management and organizational skills.
+`,
+    technologies: ["HTML", "Javascript", "React","Tailwind CSS", "Vite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "React Portfolio Website",
+    image: project1,
+    description:
+      "Built a personal portfolio website using React.js and leveraging Tailwind CSS and animated it with Framer Motion, achieving seamless mobile-to-desktop transitions and reduced page load times by 40% through optimized component rendering. Showcases myself, projects, and experience.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Huffman Code Generator",
+    image: project2,
+    description:
+      "A data compression system using Huffman coding algorithm, implementing custom binary trees for efficient character encoding– achieved 60% file size reduction through Huffman compression.",
+    technologies: ["C++","Binary Trees", "Priority Queues", "Algorithm Optimization"," Object-Oriented Programming"],
+  },
+  {
+    title: "Dijkstras Algorithm",
+    image: project3,
+    description:
+      "Led a 3 person team and developed a Graph ADT using Dijkstra's Algorithm, to compute optimal pathfinding between vertices in weighted graphs, reducing route time by 75% compared to brute force approaches and achieving O(n log n) time complexity.",
+    technologies: ["C++","Adjacency Matrix/Lists", "Algorithm Design", "Team Software Development"],
+  },
+  {
+    title: "Notation Converter",
+    image: project4,
+    description:
+      "Handles 1000+ expressions/second with 100% accuracy rate across Infix, Prefix, and Postfix conversions. Also built a Deque from scratch using doubly linked lists to handle expression parsing and conversion.",
+    technologies: ["C++","Deque, Linked Lists", "Algorithm Design", "Object-Oriented Programming", "Expression Parsing"],
+  },
+];
+
+export const CONTACT = {
+  address: "Tampa, Florida ",
+  phoneNo: "+1 (813) 468-8293 ",
+  email: "layalie.abuoleim@gmail.com",
+};
