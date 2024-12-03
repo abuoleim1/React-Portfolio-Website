a1a7828f609a12b405b076d158751f960738614f
Layalie AbuOleim <143292520+abuoleim1@users.noreply.github.com>
2024-12-03 14:32:41 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,27 @@
 import logo from "../assets/kevinRushLogo.png";
-import {FaLinkedin} from "react-icons/fa";
-import {FaGithub} from "react-icons/fa";
-import {FaInstagram} from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
-const Navbar =() => {
+const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
                 <img className="mx-2 w-10" src={logo} alt="logo" />
             </div>
-            <div className="m-8 flex items-center justify-center gap-4 text -2xl">
-                <FaLinkedin />
-                <FaGithub />
-                <FaInstagram />
+            <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+                <a href="https://www.linkedin.com/in/layalie-abuoleim" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+                <a href="https://www.github.com/abuoleim1" target="_blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </a>
+                <a href="https://www.instagram.com/layalieabuoleim/profilecard/?igsh=ZXRuaXNhNHh2NGl2" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram />
+                </a>
             </div>
-
         </nav>
     );
-
 };
+
 export default Navbar;
